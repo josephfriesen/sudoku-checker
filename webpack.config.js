901ccb4a1b2ab9a5e8c0e278206db73f2b98b119
Chris Gosser and Joe Friesen <chris+joe@>
2018-10-02 11:43:15 -0700

    template

--- webpack.config.js
+++ webpack.config.js
@@ -55,26 +55,25 @@ module.exports = {
         options: {
           presets: ['es2015']
         }
-      }
+      },
       {
-  test: /\.(gif|png|jpe?g)$/,
-  use: [
-    {
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]',
-        outputPath: 'assets/images/'
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
       }
-    }
-  ]
-},
-
-{
-  test:/\.html$/,
-  use: [
-    'html-loader'
-  ]
-},
     ]
   }
 };
